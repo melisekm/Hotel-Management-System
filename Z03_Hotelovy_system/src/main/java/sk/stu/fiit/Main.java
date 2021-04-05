b4cd0709282e3cab7b882d042a32d92a82763e8d
melisekm <46221266+melisekm@users.noreply.github.com>
2021-04-05 12:02:15 +0200

    maven, mvc init

---
+++ Z03_Hotelovy_system/src/main/java/sk/stu/fiit/Main.java
@@ -0,0 +1,13 @@
+package sk.stu.fiit;
+
+import sk.stu.fiit.view.MainScreen;
+
+/**
+ *
+ * @author Martin Melisek
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainScreen.main();
+    }
+}
